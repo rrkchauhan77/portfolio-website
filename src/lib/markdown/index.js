9a9a290ace65dc8d130866e977f3b328656828da
Rakesh Kumar <Rrkchauhan77@gmail.com>
2024-07-27 16:25:09 +0530

    Removed default markdown

--- src/lib/markdown/index.js
+++ src/lib/markdown/index.js
@@ -1,10 +1,4 @@
-import ExpenseCalculatorMD from "./expense-calculator.md";
-import BidmiiMD from "./bidmii.md";
-
-export const MarkdownFilesMap = {
-  1: BidmiiMD,
-  2: ExpenseCalculatorMD,
-};
+export const MarkdownFilesMap = {};
 
 export const fetchMarkDownFile = (id) => {
   const filePath = MarkdownFilesMap[id];
